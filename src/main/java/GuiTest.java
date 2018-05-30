9e5b99614fd280ec9d989b49319a58865197d7f0
lmlemonfish <547654784@qq.com>
2018-05-30 21:40:35 +0800

    项目搭建frist

---
+++ src/main/java/GuiTest.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GuiTest {
+    private JList list1;
+    private JPanel panel1;
+
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
