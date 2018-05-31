6f4dfc9b8e1b7a544ee7de084f2701994169f5c9
lmlemonfish <547654784@qq.com>
2018-05-31 16:12:21 +0800

    form测试

--- src/main/java/GuiTest.java
+++ src/main/java/GuiTest.java
@@ -1,11 +1,20 @@
 import javax.swing.*;
 
 public class GuiTest {
-    private JList list1;
-    private JPanel panel1;
+    private JPanel mainPanel;
+    private JTabbedPane tabbedPane1;
+    private JButton button1;
+    private JTextArea logArea;
 
+    public GuiTest() {
+        //mainPanel.setSize(800,800);
+    }
 
     public static void main(String[] args) {
-        System.out.println("hello word");
+        JFrame frame = new JFrame("GuiTest");
+        frame.setContentPane(new GuiTest().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
