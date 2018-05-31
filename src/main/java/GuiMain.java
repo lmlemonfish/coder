765d581ebf4f44b88d11cef78e5d14d12172fad8
lmlemonfish <547654784@qq.com>
2018-05-31 16:37:24 +0800

    resources , test

--- src/main/java/GuiMain.java
+++ src/main/java/GuiMain.java
@@ -1,18 +1,18 @@
 import javax.swing.*;
 
-public class GuiTest {
+public class GuiMain {
     private JPanel mainPanel;
     private JTabbedPane tabbedPane1;
     private JButton button1;
     private JTextArea logArea;
 
-    public GuiTest() {
+    public GuiMain() {
         //mainPanel.setSize(800,800);
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("GuiTest");
-        frame.setContentPane(new GuiTest().mainPanel);
+        JFrame frame = new JFrame("GuiMain");
+        frame.setContentPane(new GuiMain().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
